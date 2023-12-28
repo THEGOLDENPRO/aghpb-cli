87db9275acd9de16ce65d39b7ce71595264a66a9
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-12-28 22:12:47 +0000

    fix, feat: Fixed directory, added --last flag and code refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use bytes::Bytes;
-use std::{env::{self, var}, process::Command, error::Error, io::{self, Write, BufRead}, fs, num::IntErrorKind};
-use owo_colors::colors::{*, css::{OrangeRed, MediumPurple}, xterm::PurplePizzazz};
+use std::{env::{self}, error::Error, num::IntErrorKind, fs};
+use owo_colors::colors::{*, css::OrangeRed, xterm::PurplePizzazz};
+use utils::{check_dir, fix_win_colour, input, get_path, display_image};
+
+mod utils;
 
 const TEMP_DIR: &str = "/aghpb-cli";
 const TEMP_BOOK_NAME: &str = "/-_-.png";
@@ -10,6 +12,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 const HELP_MSG: &str = "
 USAGE: aghpb-cli {query} {limit}
 
+--last: Displays the last image you queried.
 --version: Shows current version.
 --help: Shows this message.
 ";
@@ -46,7 +49,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let chosen_book = &books[choice.parse::<usize>().expect("Failed to parse your choice into an integer.") - 1];
         let chosen_book = chosen_book.get_book().await.expect("Failed to get book's image!");
 
-        display_book(chosen_book.raw_bytes);
+        println!("{}", owo_colors::OwoColorize::fg::<BrightWhite>(&"Writing image..."));
+        let image_path = get_path(Some(TEMP_BOOK_NAME));
+        fs::write(&image_path, chosen_book.raw_bytes).expect("Failed to write book to disk!");
+
+        display_image(image_path.into());
     }
 
     Ok(())
@@ -78,13 +85,20 @@ fn parse_query(mut cmd_args: Vec<String>) -> (Option<String>, u8) {
         cmd_args[1..].join(" ")
     };
 
-    if query == "--help" {
-        println!("{}", HELP_MSG);
-        return (None, limit);
-    }
+    if query.contains("--") {
 
-    if query == "--version" {
-        println!("{} --> {}", owo_colors::OwoColorize::fg::<BrightBlue>(&"Version"), owo_colors::OwoColorize::fg::<OrangeRed>(&VERSION));
+        if query == "--help" {
+            println!("{}", HELP_MSG);
+        } else if query == "--version" {
+            println!(
+                "{} --> {}", 
+                owo_colors::OwoColorize::fg::<BrightBlue>(&"Version"), 
+                owo_colors::OwoColorize::fg::<OrangeRed>(&VERSION)
+            );
+        } else if query == "--last" {
+            display_image(get_path(Some(TEMP_BOOK_NAME)));
+        }
+
         return (None, limit);
     }
 
@@ -93,65 +107,5 @@ fn parse_query(mut cmd_args: Vec<String>) -> (Option<String>, u8) {
         parse_query(cmd_args)
     } else {
         (Some(query), limit)
-    }
-}
-
-fn display_book(raw_bytes: Bytes) {
-    println!("{}", owo_colors::OwoColorize::fg::<BrightWhite>(&"Writing image..."));
-
-    fs::write(get_path(Some(TEMP_BOOK_NAME)), raw_bytes).expect("Failed to write book to disk!");
-
-    println!("{}", owo_colors::OwoColorize::fg::<MediumPurple>(&"Displaying book..."));
-
-    let process = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .arg("/c")
-            .arg(get_path(Some(TEMP_BOOK_NAME)))
-            .spawn()
-    } else { // If you want MacOS support, contribute please.
-        Command::new("xdg-open")
-            .arg(get_path(Some(TEMP_BOOK_NAME)))
-            .spawn()
-    };
-
-    process.expect("Failed to open image of book!");
-}
-
-/// Python like input function I totally didn't steal.
-fn input(prompt: String) -> io::Result<String> {
-    print!("{}", prompt);
-    io::stdout().flush()?;
-    io::stdin()
-        .lock()
-        .lines()
-        .next()
-        .unwrap()
-        .map(|x| x.trim_end().to_owned())
-}
-
-/// Function that returns temporary storage path for book images.
-fn get_path(target_file: Option<&str>) -> String {
-    let target_file = target_file.unwrap_or("".into()).to_owned();
-
-    if cfg!(target_os = "windows") {
-        var("AppData").expect("Failed to find Windows AppData environment variable!") + &(TEMP_DIR.to_owned() + &target_file).replace("/", r"\")
-    } else { // If you want MacOS support, contribute please.
-        var("HOME").expect("Failed to find HOME environment variable! Are you on Linux?") + "/.config" + &TEMP_DIR + &target_file
-    }
-}
-
-fn fix_win_colour() {
-    if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .arg("/c")
-            .arg("color")
-            .spawn()
-            .expect("Failed to enable colours in Windows 10 cmd terminal!");
-    }
-}
-
-fn check_dir() {
-    if !(fs::metadata(get_path(None)).is_ok()) {
-        fs::create_dir(get_path(None)).expect("Couldn't create temporary directory.");
     }
 }
---
+++ src/utils.rs
@@ -0,0 +1,62 @@
+use std::{process::{Command, Stdio}, io::{self, Write, BufRead}, env::var, fs};
+use owo_colors::colors::css::MediumPurple;
+
+use crate::TEMP_DIR;
+
+pub fn display_image(path: String) {
+    println!("{}", owo_colors::OwoColorize::fg::<MediumPurple>(&"Displaying book..."));
+
+    let process = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/c")
+            .arg(path)
+            .spawn()
+    } else { // If you want MacOS support, contribute please.
+        Command::new("xdg-open")
+            .arg(path)
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+    };
+
+    process.expect("Failed to open image of book!");
+}
+
+/// Python like input function I totally didn't steal.
+pub fn input(prompt: String) -> io::Result<String> {
+    print!("{}", prompt);
+    io::stdout().flush()?;
+    io::stdin()
+        .lock()
+        .lines()
+        .next()
+        .unwrap()
+        .map(|x| x.trim_end().to_owned())
+}
+
+/// Function that returns temporary storage path for book images.
+pub fn get_path(target_file: Option<&str>) -> String {
+    let target_file = target_file.unwrap_or("".into()).to_owned();
+
+    if cfg!(target_os = "windows") {
+        var("AppData").expect("Failed to find Windows AppData environment variable!") + &(TEMP_DIR.to_owned() + &target_file).replace("/", r"\")
+    } else { // If you want MacOS support, contribute please.
+        var("HOME").expect("Failed to find HOME environment variable! Are you on Linux?") + "/.cache" + &TEMP_DIR + &target_file
+    }
+}
+
+pub fn fix_win_colour() {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/c")
+            .arg("color")
+            .spawn()
+            .expect("Failed to enable colours in Windows 10 cmd terminal!");
+    }
+}
+
+pub fn check_dir() {
+    if !(fs::metadata(get_path(None)).is_ok()) {
+        fs::create_dir(get_path(None)).expect("Couldn't create temporary directory.");
+    }
+}
